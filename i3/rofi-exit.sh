9ccfca924cad56be08fcc0f4abca7405513a4ba4
Contree111 <89882544+Contree111@users.noreply.github.com>
2025-06-03 08:03:15 -0400

    Add files via upload

---
+++ i3/rofi-exit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Use Rofi to show the menu
+chosen=$(echo -e "Logout\nReboot\nShutdown\nCancel" | rofi -dmenu -i -p "Exit Menu")
+
+case "$chosen" in
+    Logout)
+        i3-msg exit
+        ;;
+    Reboot)
+        systemctl reboot
+        ;;
+    Shutdown)
+        systemctl poweroff
+        ;;
+    Cancel)
+        exit 0
+        ;;
+    *)
+        exit 1
+        ;;
+esac
+
